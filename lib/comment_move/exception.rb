9ddbfaeaa653eea6a537b2f358767c5b843e9452
Masaya Takeda <844196@gmail.com>
2015-10-02 22:28:39 +0900

    例外追加
    InvalidThreadNumber

--- lib/comment_move/exception.rb
+++ lib/comment_move/exception.rb
@@ -2,4 +2,5 @@ module CommentMove
   class NetworkError < StandardError; end
   class InvalidLiveVideoNumber < StandardError; end
   class ExpiredUserSession < StandardError; end
+  class InvalidThreadNumber < StandardError; end
 end
