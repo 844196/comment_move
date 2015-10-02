945e403fb1346b8cee2cc12a82c49eb1eb996dc5
Masaya Takeda <844196@gmail.com>
2015-10-02 22:29:09 +0900

    waybackkeyが取得出来なかった場合の例外処理を追加

--- lib/comment_move/get_waybackkey.rb
+++ lib/comment_move/get_waybackkey.rb
@@ -1,3 +1,4 @@
+require_relative 'exception'
 require 'net/https'
 require 'nokogiri'
 
@@ -5,6 +6,11 @@ module CommentMove
   def get_waybackkey(thread, user_session)
     url = Net::HTTP.new('watch.live.nicovideo.jp')
     res = url.get("/api/getwaybackkey?thread=#{thread}", {'Cookie' => "user_session=#{user_session}"})
-    res.body[/waybackkey=(.+)/, 1]
+
+    unless waybackkey = res.body[/waybackkey=(.+)/, 1]
+      raise InvalidThreadNumber
+    end
+
+    waybackkey
   end
 end
